48f74e920d0f6335684baa7a21bf4e2238ff3b1e
Ivan <sembiznet@gmail.com>
2024-11-04 19:17:45 +0700

    ispav_zamech

--- 1-concurrency/dz_1.go
+++ 1-concurrency/dz_1.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 )
 
 // начало программы
@@ -12,16 +13,23 @@ func main() {
 	z := 0
 	var array []int
 	var array2 []int
-	go func() {
+	var wg sync.WaitGroup
 
+	//генерация случайных чисел
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
 		for i := 0; i < 10; i++ {
 			z = rand.Intn(100) + 1
 			ch <- z
 			array = append(array, z)
 		}
+		close(ch)
 	}()
-
+	//возводим в квадрат
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		b := 0
 		for i := 0; i < 10; i++ {
 			b = <-ch
@@ -29,8 +37,9 @@ func main() {
 			array2 = append(array2, b)
 			ch2 <- b
 		}
+		close(ch2)
 	}()
-
+	//выводим результат
 	for i := 0; i < 10; i++ {
 		fmt.Printf("  %d", <-ch2)
 	}
